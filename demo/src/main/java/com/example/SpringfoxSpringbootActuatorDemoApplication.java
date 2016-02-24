1b58f126f38e36378372a34536b2609a8a861edb
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-02-25 06:11:05 +0900

    restructured

--- demo/src/main/java/com/example/SpringfoxSpringbootActuatorDemoApplication.java
+++ demo/src/main/java/com/example/SpringfoxSpringbootActuatorDemoApplication.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import com.google.common.collect.Sets;
-import io.swagger.annotations.*;
 import me.geso.swagger2_actuator.EnableSwagger2Actuator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,30 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @EnableSwagger2Actuator
-@SwaggerDefinition(
-        info = @Info(
-                description = "Gets the weather",
-                version = "V12.0.12",
-                title = "The Weather API",
-                termsOfService = "http://theweatherapi.io/terms.html",
-                contact = @Contact(
-                        name = "Rain Moore",
-                        email = "rain.moore@theweatherapi.io",
-                        url = "http://theweatherapi.io"
-                ),
-                license = @License(
-                        name = "Apache 2.0",
-                        url = "http://www.apache.org/licenses/LICENSE-2.0"
-                )
-        ),
-        consumes = {"application/json", "application/xml"},
-        produces = {"application/json", "application/xml"},
-        schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
-        tags = {
-                @Tag(name = "Private", description = "Tag used to denote operations as private")
-        },
-        externalDocs = @ExternalDocs(value = "Meteorology", url = "http://theweatherapi.io/meteorology.html")
-)
 public class SpringfoxSpringbootActuatorDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringfoxSpringbootActuatorDemoApplication.class, args);
